b8245408ef6e0115b2762ade209074bba60fdf8b
Hosein <ho3ein.khajeh8282@gmail.com>
2023-12-20 04:05:45 +0330

    make sure1

--- Tamrin.java
+++ Tamrin.java
@@ -1,10 +1,12 @@
 package exeercise;
+
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class Tamrin {
-//   -------------------------------------------- gereftan matrix
-    public  static int[][] get_matrix(int n) {
-    System.out.println("---------------------------");
+    // -------------------------------------------- gereftan matrix
+    public static int[][] get_matrix(int n) {
+        System.out.println("---------------------------");
         System.out.println("Moalefe hara vared koni:  ");
         Scanner input = new Scanner(System.in);
         int[][] matrix = new int[n][n];
@@ -15,29 +17,35 @@ public class Tamrin {
         }
         return matrix;
     }
-    //  ----------------------------------------  khod maghloob
-    public static String maghloob(int y,int l) {
+
+    // ---------------------------------------- khod maghloob
+    public static String maghloob(int y, int l) {
         int n2 = y;
-        int m=0;
+        int m = 0;
         int s = 0;
-        String sl ="";
+        String sl = "";
         while (y > 0) {
-            m=  y % 10;
+            m = y % 10;
             s = (s * 10) + m;
             y = y / 10;
-        }if (n2 == s) {
-             sl = "True";
-        }return sl;
+        }
+        if (n2 == s) {
+            sl = "True";
+        }
+        return sl;
     }
-// --------------------------------------------- show array
-    public static void showArray(int[][] m){
-    System.out.println("---------------------------");
+
+    // --------------------------------------------- show array
+    public static void showArray(int[][] m) {
+        System.out.println("---------------------------");
         System.out.println("Your input array is :");
-        for (int[] i : m){
+        for (int[] i : m) {
             System.out.println(Arrays.toString(i));
-        }System.out.println("---------------------------");
+        }
+        System.out.println("---------------------------");
     }
-//    -----------------------------------------andis gozari
+
+    // -----------------------------------------andis gozari
     public static int[][] showspiralArray(int dimension) {
         int[][] spiralArray = new int[dimension][dimension];
         int numConcentricSquares = (int) Math.ceil((dimension) / 2.0);
@@ -58,26 +66,28 @@ public class Tamrin {
                 spiralArray[i + j][i] = currNum++;
             }
             sideLen -= 2;
-            }return spiralArray;
+        }
+        return spiralArray;
     }
-//    ---------------------------final mohasebati
-    public static void nahayi(int tedad_line,int[][] showSprial,int[][] matrix){
+
+    // ---------------------------final mohasebati
+    public static void nahayi(int tedad_line, int[][] showSprial, int[][] matrix) {
         Scanner input = new Scanner(System.in);
         int z = 0;
         int count = 0;
         System.out.println("khane mored nazar ra vared konid:  ");
-        int x = input.nextInt() ;
+        int x = input.nextInt();
         System.out.println("--------------------------------------");
         int[][] arrayAdadi = showspiralArray(tedad_line);
+        // mitonim mosavi bezarim vali eror mide------------------------------
         for (int i = 0; i < arrayAdadi.length; i++) {
             for (int j = 0; j < arrayAdadi.length; j++) {
                 for (int l = 0; l < x; l++) {
                     if (l == arrayAdadi[i][j]) {
                         z += matrix[i][j];
                         ;
-                        if (maghloob(z,l)=="True"){
-                            System.out.println("Adad " + z + " dar andis "+l+" maghloob ast.");
-
+                        if (maghloob(z, l) == "True") {
+                            System.out.println("Adad " + z + " dar andis " + l + " maghloob ast.");
                             count++;
                         }
                     }
@@ -85,17 +95,18 @@ public class Tamrin {
             }
         }
         System.out.println("--------------------------------------");
-        System.out.println("Tedad kol maghloob ha barabar ast ba : "+count);
+        System.out.println("Tedad kol maghloob ha barabar ast ba : " + count);
     }
-    //    ------------------------------------------------ main
+
+    // ------------------------------------------------ main
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Tedad satr va soton ra benevisid:  ");
         int n = input.nextInt();
-        int[][] matrix =  get_matrix(n);
-        int[][] show =  showspiralArray(n);
+        int[][] matrix = get_matrix(n);
+        int[][] show = showspiralArray(n);
         showArray(matrix);
-        nahayi(n,show,matrix);
+        nahayi(n, show, matrix);
 
     }
 }
--- mainTamrinHalazoon.java
+++ mainTamrinHalazoon.java
@@ -7,9 +7,9 @@ public class mainTamrinHalazoon {
         Scanner input = new Scanner(System.in);
         System.out.println("Tedad satr va soton ra benevisid:  ");
         int n = input.nextInt();
-        int[][] matrix =  Tamrin.get_matrix(n);
-        int[][] show =  Tamrin.showspiralArray(n);
+        int[][] matrix = Tamrin.get_matrix(n);
+        int[][] show = Tamrin.showspiralArray(n);
         Tamrin.showArray(matrix);
-        Tamrin.nahayi(n,show,matrix);
+        Tamrin.nahayi(n, show, matrix);
     }
 }
